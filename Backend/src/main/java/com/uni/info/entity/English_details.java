1a560c022d27dcb06c2fb450eeaa361ad82ad2ed
Nilushiya <nilushiyak@gmail.com>
2024-05-04 20:31:30 +0530

    EnglishDetails api all created

--- Backend/src/main/java/com/uni/info/entity/English_details.java
+++ Backend/src/main/java/com/uni/info/entity/English_details.java
@@ -2,18 +2,20 @@ package com.uni.info.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.w3c.dom.Text;
 
 @Entity
 @Data
-public class EnglishDetails {
+@Table(name = "EnglishDetails")
+public class english_details {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long edetails_id;
-    private Integer edegreeDuration;
-    @Lob
-    private byte[] imageData;
-    private String edegreeDescription1;
-    private String edegreeJobs;
+    private Integer edegree_duration;
+//    @Lob
+//    private byte[] image_data;
+    private String edegree_description1;
+    private String edegree_jobs;
     private Long edeg_id;
 //    @ManyToOne
 //    @JoinColumn(name = "edeg_id", referencedColumnName = "degree_id")
---
+++ Backend/src/main/java/com/uni/info/repository/StudentInfoRepo.java
@@ -0,0 +1,2 @@
+package com.uni.info.repository;public interface StudentInfoRepo {
+}
