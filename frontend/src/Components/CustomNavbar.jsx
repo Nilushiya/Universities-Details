56c880b93c5612e6e3bd195b8ae143c0c963934a
Nilushiya <nilushiyak@gmail.com>
2024-05-07 09:52:49 +0530

    Navbar frontend okay

--- frontend/src/Components/CustomNavbar.jsx
+++ frontend/src/Components/CustomNavbar.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Navbar as BootstrapNavbar, Nav, NavDropdown } from 'react-bootstrap';
+import brand from './Assets/Brand.png'
+import { navbar } from './Style';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 
 const CustomNavbar = () => {
+  const token = null;
   return (
-    <BootstrapNavbar bg="light" expand="lg">
-      <BootstrapNavbar.Brand href="#home">Navbar</BootstrapNavbar.Brand>
-      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
-      <BootstrapNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+    <BootstrapNavbar  expand="lg" id='nav'>
+      <BootstrapNavbar.Brand href="/" className='brand'><img src={brand} alt="" /></BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" style={{ backgroundColor:'#ff5b25'}}/>
+      <BootstrapNavbar.Collapse id="basic-navbar-nav" >
+        <Nav className="me-auto" >
+          <NavDropdown title="Universities" id="basic-nav-dropdown" className='uniDropdown'>
+            <NavDropdown.Item href="#action/3.1" className='uniDropdownItem'>Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2" className='uniDropdownItem'>Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3" className='uniDropdownItem'>Something</NavDropdown.Item>
           </NavDropdown>
+        </Nav>
+        <Nav className='flexRight'>{token ? 
+              <Nav.Link className='navbutt' href='/login'>
+                Login
+              {/* <button  className='navBtn'>Login</button> */}
+            </Nav.Link>
+            : <Nav.Link className='navbutt' href='/register'>
+              SignUp
+            {/* <button  className='navBtn'>SignUp</button> */}
+          </Nav.Link>
+            }
+          <Nav.Link className='navicon' href={token ? '/profile'  : '/login'}>
+            {
+              <FontAwesomeIcon icon={faUser} color='#ff5b25' size='xl'/> 
+            }
+          </Nav.Link>
+          <Nav.Link className='navicon navFriend' href={token ? '/friend' : '/login'}>
+            {
+              <FontAwesomeIcon icon={faUserGroup}  className='friendIcon' size='xl'/> 
+            }
+          </Nav.Link>
         </Nav>
       </BootstrapNavbar.Collapse>
     </BootstrapNavbar>
